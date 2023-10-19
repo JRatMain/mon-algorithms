1e98d4361b636d78089dedcc74aaf4f970a60800
Matthew Vrbka <matthewjvrbka@gmail.com>
2023-10-19 12:50:53 -0500

    finished the first method. starting on the second later today.

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
+import csv
+import time
+import re
+
+mons = []
 
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def max_total(reader):
+    global mons
+    max = 0
+    for row in mons:
+        num = row[0]
+        name = row[1]
+        t1 = row[2]
+        t2 = row[3]
+        total = row[4]
+        hp = row[5]
+        atk = row[6]
+        defense = row[7]
+        spatk = row[8]
+        spdef = row[9]
+        spd = row[10]
+        gen = row[11]
+        legend = bool(row[12])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def check_type_psn(reader):
+    global mons
+    print("The following Pokémon have the Poison type: ")
+    print()
+    for row in reader:
+        t1 = row[2]
+        t2 = row[3]
+        legend = bool(row[12])
+        if t1 == 'Poison' or t2 == 'Poison':
+            print(row[1])
+        mons.append(row)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+with open('Pokemon_numerical.csv') as mon:
+    if __name__ == '__main__':
+        reader = csv.reader(mon)
+        check_type_psn(reader)
+        max_total(reader)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
