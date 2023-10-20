356fb58f3059bd738e76195b2a4ecb9d581b7d57
Matthew Vrbka <matthewjvrbka@gmail.com>
2023-10-20 10:29:14 -0500

    still trying to fix the results list

--- main.py
+++ main.py
@@ -12,53 +12,35 @@ def print_results():
     for row in results:
         name = row[0]
         total = row[1]
-        print(name + ', ' + total)
+        print(name + ', ' + str(total))
 
 
-def binary_search(mons):
-    global results
-    for row in mons:
-        low = 0
-        high = len(mons) - 1
-        mid = 0
-        max_val = int(0)
-        while low <= high:
-            mid = (high + low) // 2
-            current_val = int(mons[mid][4])
+def binary_search():
+    global results, mons
+    low = 0
+    high = len(mons) - 1
+    max_val = int(0)
+    while low <= high:
+        mid = (high + low) // 2
+        current_val = int(mons[mid][4])
 
-            # If x is greater, ignore left half
-            if current_val < max_val:
-                low = mid + 1
+        # If x is greater, ignore left half
+        if current_val < max_val:
+            low = mid + 1
 
             # If x is smaller, ignore right half
-            elif current_val > max_val:
-                high = mid - 1
-                max_val = current_val
-                results.clear()
-                results.append([mons[mid][1], str(current_val)])
-            elif current_val == max_val:
-                high = mid - 1
-                name = mons[mid][1]
-                total = mons[mid][4]
-                results.append([name, total])
+        elif current_val > max_val:
+            high = mid - 1
+            max_val = current_val
+            results = [[mons[mid][1], current_val]]
+        elif current_val == max_val:
+            name = mons[mid][1]
+            total = mons[mid][4]
+            print(name + ', ' + total)
+            new_mon = [name, total]
+            results.append(new_mon)
+            high = mid - 1
 
-    print(max_val)
-
-
-# If we reach here, then the element was not present
-
-
-def max_total():
-    global mons
-    max = 0
-    for row in mons:
-        num = row[0]
-        name = row[1]
-        print(row)
-        total = int(row[4])
-
-        if total > max:
-            max = total
 
 
 def sort():
@@ -76,7 +58,6 @@ def check_type_psn(reader):
         name = row[1]
         t1 = row[2]
         t2 = row[3]
-        legend = bool(row[12])
         if t1 == 'Poison' or t2 == 'Poison':
             print(name)
         mons.append(row)
@@ -87,7 +68,7 @@ with open('Pokemon_numerical.csv') as mon:
         reader = csv.reader(mon)
         check_type_psn(reader)
         sort()
-        binary_search(mons)
+        binary_search()
         print_results()
 
     # See PyCharm help at https://www.jetbrains.com/help/pycharm/
